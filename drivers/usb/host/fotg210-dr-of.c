7ace4376e0ffff441899d393f4f08ea6fa6eb2ee
Linus Walleij <linus.walleij@linaro.org>
2017-04-21 22:40:57 +0200

    usb: host: fotg2: add Gemini-specific handling
    The Cortina Systems Gemini has bolted on a PHY inside the
    silicon that can be handled by six bits in a MISC register in
    the system controller.
    
    If we are running on Gemini, look up a syscon regmap through
    a phandle and enable VBUS and optionally the Mini-B connector.
    
    If the device is flagged as "wakeup-source" using the standard
    DT bindings, we also enable this in the global controller for
    respective port.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/usb/host/fotg210-dr-of.c
+++ drivers/usb/host/fotg210-dr-of.c
@@ -20,6 +20,11 @@
 #include <linux/module.h>
 #include <linux/dma-mapping.h>
 #include <linux/fotg210_of.h>
+#include <linux/bitops.h>
+/* For Cortina Gemini */
+#include <linux/mfd/syscon.h>
+#include <linux/regmap.h>
+
 
 struct fotg210_dev_data {
 	char *dr_mode;		/* controller mode */
@@ -105,9 +110,77 @@ error:
 
 static const struct of_device_id fotg210_dr_of_match[];
 
+/*
+ * Gemini-specific initialization function, only executed on the
+ * Gemini SoC using the global misc control register.
+ */
+#define GEMINI_GLOBAL_MISC_CTRL		0x30
+#define GEMINI_MISC_USB0_WAKEUP		BIT(14)
+#define GEMINI_MISC_USB1_WAKEUP		BIT(15)
+#define GEMINI_MISC_USB0_VBUS_ON	BIT(22)
+#define GEMINI_MISC_USB1_VBUS_ON	BIT(23)
+#define GEMINI_MISC_USB0_MINI_B		BIT(29)
+#define GEMINI_MISC_USB1_MINI_B		BIT(30)
+
+static int fotg210_gemini_init(struct device *dev, struct resource *res)
+{
+	struct device_node *np = dev->of_node;
+	struct regmap *map;
+	bool mini_b;
+	bool wakeup;
+	u32 mask, val;
+	int ret;
+
+	map = syscon_regmap_lookup_by_phandle(np, "syscon");
+	if (IS_ERR(map)) {
+		dev_err(dev, "no syscon\n");
+		return PTR_ERR(map);
+	}
+	mini_b = of_property_read_bool(np, "cortina,gemini-mini-b");
+	wakeup = of_property_read_bool(np, "wakeup-source");
+
+	/*
+	 * Figure out if this is USB0 or USB1 by simply checking the
+	 * physical base address.
+	 */
+	mask = 0;
+	if (res->start == 0x69000000) {
+		val = GEMINI_MISC_USB1_VBUS_ON;
+		if (mini_b)
+			val |= GEMINI_MISC_USB1_MINI_B;
+		else
+			mask |= GEMINI_MISC_USB1_MINI_B;
+		if (wakeup)
+			val |= GEMINI_MISC_USB1_WAKEUP;
+		else
+			mask |= GEMINI_MISC_USB1_WAKEUP;
+	} else {
+		val = GEMINI_MISC_USB0_VBUS_ON;
+		if (mini_b)
+			val |= GEMINI_MISC_USB0_MINI_B;
+		else
+			mask |= GEMINI_MISC_USB0_MINI_B;
+		if (wakeup)
+			val |= GEMINI_MISC_USB0_WAKEUP;
+		else
+			mask |= GEMINI_MISC_USB0_WAKEUP;
+	}
+
+	ret = regmap_update_bits(map, GEMINI_GLOBAL_MISC_CTRL, mask, val);
+	if (ret) {
+		dev_err(dev, "failed to initialize Gemini PHY\n");
+		return ret;
+	}
+
+	dev_info(dev, "initialized Gemini PHY\n");
+	return 0;
+}
+
+
 static int fotg210_of_probe(struct platform_device *ofdev)
 {
 	struct device_node *np = ofdev->dev.of_node;
+	struct device *dev = &ofdev->dev;
 	struct platform_device *usb_dev;
 	struct fotg210_platform_data data, *pdata;
 	struct fotg210_dev_data *dev_data;
@@ -121,6 +194,16 @@ static int fotg210_of_probe(struct platform_device *ofdev)
 	match = of_match_device(fotg210_dr_of_match, &ofdev->dev);
 	if (!match)
 		return -ENODEV;
+
+	if (of_device_is_compatible(dev->of_node, "cortina,gemini-usb")) {
+		struct resource *res;
+		int retval;
+
+		res = platform_get_resource(ofdev, IORESOURCE_MEM, 0);
+		retval = fotg210_gemini_init(dev, res);
+		if (retval)
+			return -ENODEV;
+	}
 
 	pdata = &data;
 	if (match->data)
